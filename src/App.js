668aed0462e43c0e95ded03f02b06b9ebcd07e07
salih <salihkilinc80@gmail.com>
2023-01-04 20:31:13 +0100

    v1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import HelloWorld from "./components/01.hello-world/hello-world"
+//import HelloReact from "./components/hello-react/hello-react";
+
+function App() {
+  return (
+    <div>
+      <HelloWorld/>
+       <HelloWorld/>
+        <HelloWorld/>
+
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/01.hello-world/hello-world.js
@@ -0,0 +1,10 @@
+import HelloReact from "../hello-react/hello-react";
+
+const HelloWorld = () =>{
+    return(
+       <div>Hello world
+        <HelloReact/>
+       </div>
+    )
+}
+export default HelloWorld;
